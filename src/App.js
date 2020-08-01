ac3116b265518e3385c5357e8e4fba27a31d720b
Guillaume Laine <guillaumelaine2.0@gmail.com>
2020-08-01 18:40:33 +0200

    fetch nasa apod ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const [photo, setPhoto] = React.useState();
+
+  React.useEffect(() => {
+    const fetchRandomPhoto = async () => {
+      const response = await fetch("./.netlify/functions/images");
+      if (response.ok) {
+        const photo = await response.json();
+        setPhoto(photo);
+      }
+    };
+    fetchRandomPhoto();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {photo && <img alt={photo.alt_description} src={photo.url} />}
     </div>
   );
 }
